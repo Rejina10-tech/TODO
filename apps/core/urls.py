6f3549e2f9980fdb74f7b1ca73a50f472c111fcf
unknown <rezeena.dahal@gmail.com>
2024-11-19 13:44:52 +0400

    feat: implement JWT authentication

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.views import TodoViewSet
+from .views.logout import LogoutView
+from .views.reset_pass import PasswordResetRequestView, PasswordResetConfirmView
+from .views.register import RegisterAPIView
+
+
+
 
 router = DefaultRouter()
 router.register(r'tasks', TodoViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/logout/', LogoutView.as_view(), name='logout'),
+     path('api/register/', RegisterAPIView.as_view(), name='register'),
+    path('api/password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('api/password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
---
+++ apps/core/views/register.py
@@ -0,0 +1,29 @@
+
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+
+from apps.core.serializers.user import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class RegisterAPIView(APIView):
+    permission_classes = [AllowAny] 
+    def post(self, request):
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            return Response({
+                "message": "User registered successfully",
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+                "user": user_serializer.data
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
